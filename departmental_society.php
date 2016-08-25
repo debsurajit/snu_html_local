80fcee2a660b90347628de97c9f8b6c960aca237
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-08-25 11:07:28 +0530

    small-changes

--- departmental_society.php
+++ departmental_society.php
@@ -15,28 +15,83 @@
           <li><a>Contact</a></li>
         </ul>
       </div>
+      <ul class="leftmenu-bottom">
+        <li><a href="#">Interested in us?</a></li>
+        <li><a href="#">Check out our Blog</a></li>
+      </ul>
     </div>
     <!-- Left Panel End -->
 
     <!-- Middle Panel Start -->
     <div class="col-sm-9">
-      
+
       <!-- Banner Start -->
-  <div class="banner">
-    <img src="images/slide1.jpg" alt="">
-  </div>
-  <!-- Banner End -->
-      
-      
+
+      <div class="banner row">
+        <img src="images/slide1.jpg" alt="">
+      </div>
+      <!-- Banner End -->
+
+
       <div class="row">
         <div class="col-sm-8 middle-content">
           <h1>Orion - The Civil Society</h1>
+          <img src="images/logo-img.jpg" alt="" class="pull-left">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+          <img src="images/video-img.jpg" alt="">
+
+          <h2 class="title">Meet the Team</h2>
+
+          <div class="row">
+            <div class="col-sm-4">
+              <img src="images/team1.jpg" alt="">
+              <h3>Nellie Holland</h3> Secretary
+              <a href="mailto:jaideep.chaterjee@snu.edu.in">jaideep.chaterjee@snu.edu.in</a>
+            </div>
+            <div class="col-sm-4">
+              <img src="images/team2.jpg" alt="">
+              <h3>Nellie Holland</h3> Secretary
+              <a href="mailto:jaideep.chaterjee@snu.edu.in">jaideep.chaterjee@snu.edu.in</a>
+            </div>
+            <div class="col-sm-4">
+              <img src="images/team3.jpg" alt="">
+              <h3>Nellie Holland</h3> Secretary
+              <a href="mailto:jaideep.chaterjee@snu.edu.in">jaideep.chaterjee@snu.edu.in</a>
+            </div>
+            <div class="col-sm-4">
+              <img src="images/team4.jpg" alt="">
+              <h3>Nellie Holland</h3> Secretary
+              <a href="mailto:jaideep.chaterjee@snu.edu.in">jaideep.chaterjee@snu.edu.in</a>
+            </div>
+            <div class="col-sm-4">
+              <img src="images/team5.jpg" alt="">
+              <h3>Nellie Holland</h3> Secretary
+              <a href="mailto:jaideep.chaterjee@snu.edu.in">jaideep.chaterjee@snu.edu.in</a>
+            </div>
+          </div>
         </div>
         <!-- Middle Panel End -->
 
         <!-- Right Panel Start -->
         <div class="col-sm-4 right-panel">
-          RIGHT
+
+          <img src="images/right-img1.jpg" alt="" width="100%">
+          <div class="events">
+            <div class="date">06 May 2016</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            <div class="date">06 May 2016</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+
+          <div class="events upcoming">
+            <h4>Upcoming Events</h4>
+            <div class="date">06 May 2016</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            <div class="date">06 May 2016</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            <div class="date">06 May 2016</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
         </div>
         <!-- Right Panel End -->
       </div>
@@ -46,4 +101,4 @@
     <!-- Content End -->
   </div>
 
-<?php include 'footer.php';?>
+  <?php include 'footer.php';?>
