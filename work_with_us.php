97030062fed82c1877871ffb67dc3137103e205c
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-08-25 18:00:15 +0530

    change-animation

--- work_with_us.php
+++ work_with_us.php
@@ -23,7 +23,7 @@
             </h1>
           </div>
           <div class="animatedParent">
-            <div class="animated bounceInUp slower">
+            <div class="animated fadeIn slower">
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
             </div>
           </div>
