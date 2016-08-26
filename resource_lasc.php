5ea2f086e63e6e8bd9781d115d124833944dcb48
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-08-26 12:23:05 +0530

    resource-pages-done

---
+++ resource_lasc.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+  <!-- Content Start -->
+  <div class="container margin-top">
+
+    <div class="breadcrumb col-md-offset-3">Resource / <span>Learning & Academic Support Center</span></div>
+
+    <!-- Left Panel Start -->
+    <div class="col-sm-3">
+      <?php include 'include/resource-side-menu.php';?>
+    </div>
+    <!-- Left Panel End -->
+
+    <!-- Right Panel Start -->
+    <div class="col-sm-9">
+
+      <div class="middle-content">
+        <div class="animatedParent">
+          <h1 class="animated fadeIn slower">Learning & Academic Support Center</h1>
+        </div>
+        <div class="animatedParent">
+          <div class="animated fadeIn slower">
+            <p class="crimsone-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit. </p>
+          </div>
+        </div>
+
+        <div class="animatedParent">
+          <div class="animated fadeIn slower">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit. In nec justo arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit. In nec justo arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit. In nec justo arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+
+        <div class="animatedParent">
+          <div class="animated fadeIn slower">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit. In nec justo arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu mattis nulla. Morbi vulputate lorem vitae condimentum pharetra. Vestibulum arcu arcu, sagittis ullamcorper imperdiet et, tempor vitae quam.Sed quis fringilla sapien, ut vehicula elit. In nec justo arcu. ut vehicula elit.</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <!-- Right Panel End -->
+
+
+
+
+    </div>
+    <!-- Content End -->
+  </div>
+
+
+  <?php include 'footer.php';?>
