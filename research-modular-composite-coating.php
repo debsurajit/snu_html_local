7dbd4503f4c33ecbdb9dffa37362f2de8da46585
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-08-28 10:36:39 +0530

    modular

---
+++ research-modular-composite-coating.php
@@ -0,0 +1,70 @@
+<?php include 'header.php';?>
+
+  <!-- Content Start -->
+  <div class="container margin-top">
+      <div class="top-fix-color-bg-wrapper"><div class="top-fix-color-bg">&nbsp;</div></div>
+    <div class="breadcrumb col-md-offset-3">Research / Area of Research / <span>Modular Composite Coating</span></div>
+
+    <div class="row">
+
+      <!-- Left Panel Start -->
+      <div class="col-sm-3">
+        <?php include 'include/research-side-menu.php';?>
+          <ul class="leftmenu-bottom">
+            <li><a href="#">Research Compliances</a></li>
+          </ul>
+      </div>
+      <!-- Left Panel End -->
+
+      <!-- Middle Panel Start -->
+      <div class="col-sm-9">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-8 middle-content">
+            <div class="animatedParent">
+              <div class="animated bounceInUp slower">
+                <h1>Modular Composite Coating</h1>
+              </div>
+            </div>
+
+            <div class="animatedParent">
+              <div class="animated fadeIn slower">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+              </div>
+            </div>
+            <img src="images/modular-img.jpg" alt="">
+
+
+            <div class="animatedParent">
+              <div class="animated bounceInUp slower">
+              
+              </div>
+            </div>
+
+
+          </div>
+          <!-- Middle Panel End -->
+
+          <!-- Right Panel Start -->
+          <div class="col-lg-4 col-md-4 col-sm-4 right-panel">
+
+            <img src="images/right-img1.jpg" alt="" width="100%">
+            <div class="events">
+              <div class="date">06 May 2016</div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+              <div class="date">06 May 2016</div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            </div>
+          
+            
+            <a href="" class="button-with-white-bg">Join us if you're interested</a>
+            <button class="button-2">Library</button>
+          </div>
+          <!-- Right Panel End -->
+        </div>
+
+      </div>
+    </div>
+    <!-- Content End -->
+  </div>
+
+  <?php include 'footer.php';?>
