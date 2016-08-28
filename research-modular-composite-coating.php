cbed9fbf8424ab5363d35970cf86ba70ccdecc9f
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-08-28 12:14:38 +0530

    modular-composite-coating-page-done

--- research-modular-composite-coating.php
+++ research-modular-composite-coating.php
@@ -35,12 +35,92 @@
 
 
             <div class="animatedParent">
-              <div class="animated bounceInUp slower">
+              <div class="animated bounceInUp slower faculty-list">
               
+                <h2>Faculty</h2>
+                <ul class="row">
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor</li>
+                </ul>
+                
+                <h2>Researchers</h2>
+                <ul class="row">
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                  <li class="col-md-4 col-sm-4 col-xs-4"><span>Jimmy Andrews</span><br/>Assistant Professor<br/>Class 2016</li>
+                </ul>
+                
               </div>
             </div>
 
 
+           <div class="row">   <div class="col-sm-12 animatedParent tabdata">
+        <h3 class="animated fadeIn slower">Publication</h3>
+             <div class="breadcrumb"><span>2016</span> / 2015-14 / 2013-12</div> 
+             
+             <div class="filter"><span>Filter</span></div>
+             
+        <div class="undergraduate-inner-page-tab">
+
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation"><a href="#books" aria-controls="books" role="tab" data-toggle="tab">BOOKS & BOOK CHAPTERS</a></li>
+                <li role="presentation" class="active"><a href="#journals" aria-controls="journals" role="tab" data-toggle="tab">JOURNALS</a></li>
+                <li role="presentation"><a href="#conferences" aria-controls="conferences" role="tab" data-toggle="tab">CONFERENCES</a></li>
+            </ul>
+
+
+            <div class="tab-content undergraduate-inner-page-accordion">
+              <!-- start tab pan -->
+              <div role="tabpanel" class="tab-pane" id="books">No Content Available</div>
+              <!-- end tab pan -->
+              
+                <!-- start tab pan -->
+                <div role="tabpanel" class="tab-pane active" id="journals">
+                  
+                  <div class="pagination">
+                    <ul>
+                      <li><a href="#">01</a></li>
+                      <li><a href="#">02</a></li>
+                      <li><a href="#">03</a></li>
+                      <li><a href="#">>></a></li>
+                    </ul>
+                  </div>
+                  
+                  <p>Lorem ipsum dolor sit amet, <strong>consectetur</strong> <a href="#">adipiscing elit. Nulla aliquam fringilla eros, eget fermentum massa bibendum</a> sit amet. Sed pulvinar elementum lorem, ut mollis felis laoreet ut. In sollicitudin leo cursus tortor luctus pharetra. Maecenas ornare feugiat magna eget efficitur.</p>
+                  
+                  <p>Lorem ipsum dolor sit amet, <strong>consectetur</strong> <a href="#">adipiscing elit. Nulla aliquam fringilla eros, eget fermentum massa bibendum</a> sit amet. Sed pulvinar elementum lorem, ut mollis felis laoreet ut. In sollicitudin leo cursus tortor luctus pharetra. Maecenas ornare feugiat magna eget efficitur.</p>
+                  
+                  <p>Lorem ipsum dolor sit amet, <strong>consectetur</strong> <a href="#">adipiscing elit. Nulla aliquam fringilla eros, eget fermentum massa bibendum</a> sit amet. Sed pulvinar elementum lorem, ut mollis felis laoreet ut. In sollicitudin leo cursus tortor luctus pharetra. Maecenas ornare feugiat magna eget efficitur.</p>
+                  
+                  <p>Lorem ipsum dolor sit amet, <strong>consectetur</strong> <a href="#">adipiscing elit. Nulla aliquam fringilla eros, eget fermentum massa bibendum</a> sit amet. Sed pulvinar elementum lorem, ut mollis felis laoreet ut. In sollicitudin leo cursus tortor luctus pharetra. Maecenas ornare feugiat magna eget efficitur.</p>
+
+                    <!-- Tab panes -->
+
+
+
+             
+
+                </div>
+                <!-- end tab pan -->
+                
+              <!-- start tab pan -->
+              <div role="tabpanel" class="tab-pane" id="conferences">No Content Available</div>              
+              <!-- end tab pan -->
+
+            </div>
+
+        </div>
+             </div></div>
+            
+            
           </div>
           <!-- Middle Panel End -->
 
@@ -55,9 +135,10 @@
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             </div>
           
-            
+            <div class="academic-calender">
             <a href="" class="button-with-white-bg">Join us if you're interested</a>
             <button class="button-2">Library</button>
+          </div>
           </div>
           <!-- Right Panel End -->
         </div>
