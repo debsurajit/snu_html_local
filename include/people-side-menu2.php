1cbc5d0fc432d7214a7a7a89726f497a6db02342
surajit debnath <surajit.debnath@innoraft.com>
2016-09-05 15:06:03 +0530

    add new animation for top menu and change some css

---
+++ include/people-side-menu2.php
@@ -0,0 +1,13 @@
+<div class="left-menu">
+        <ul>
+          <li><a href="people_faculty.php">Faculty</a></li>
+          <li><a href="people_staff.php">Staff</a></li>
+          <li><a href="people_researchers.php">Researchers</a>
+            <ul>
+              <li><a href="#">Graduates</a></li>
+              <li><a href="#">Research Assistants</a></li>
+              <li><a href="#">Undergraduates</a></li>
+            </ul>
+          </li>
+        </ul>
+</div>
