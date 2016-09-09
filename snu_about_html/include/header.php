adacb3bfa8a98e1eb1993fad93aab4cebf7d9a16
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-09-09 18:47:49 +0530

    animation

--- snu_about_html/include/header.php
+++ snu_about_html/include/header.php
@@ -5,10 +5,10 @@
   <title>About</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/common.css">
-  <link rel="stylesheet" href="css/animations.css">
-  <link rel="stylesheet" href="css/vision.css.">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/animations.css">
+  <link rel="stylesheet" href="/css/vision.css">
   
  
 </head>
