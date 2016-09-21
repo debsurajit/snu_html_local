88d0445a65b56dd448defeab29584f0ba6d7777b
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-09-21 14:26:13 +0530

    history-page

--- snu_about_html/history.php
+++ snu_about_html/history.php
@@ -12,14 +12,16 @@
        
         <h1 class="animated bounceInUp slowest">History</h1>
         
-        <div class="slide">
-        <div class="img-block">2016 
-        <img src="images/history-img.jpg" alt="">
+        <div class="slide col-sm-12">
+          <div class="col-sm-5 img-block">
+            <span>2016</span> 
+            <img src="images/history-img.jpg" alt="">
         </div>
           
-        <div class="slide-content">
-        <h2>SNU Website Redesigned by Greatest Minds on Planet</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        <div class="col-sm-6 slide-content">
+        <div><h2>SNU Website Redesigned by <br/>Greatest Minds on Planet</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
         </div>
         </div>
     </div>
