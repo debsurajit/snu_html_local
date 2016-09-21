e1883c99cb7c3dbd25b0540ce81eec6d3185739a
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-09-21 10:21:34 +0530

    changes

---
+++ snu_about_html/history.php
@@ -0,0 +1,29 @@
+<?php include 'header.php';?>
+
+  
+  <!-- Body Sart -->
+  <div class="history-page">
+  <section class="fixed-pad content-wrapper">
+   
+      <div class="container top-content animatedParent">
+      <div class="breadcrumb">
+        Home / About /
+      </div>
+       
+        <h1 class="animated bounceInUp slowest">History</h1>
+        
+        2016 
+        <img src="images/history-img.jpg" alt="">
+        <h2>SNU Website Redesigned by Greatest Minds on Planet</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    </div>
+    
+    
+      
+ 
+  </section>
+  <!-- Body End -->
+  
+
+  </div>
+ <?php include 'footer.php';?>
