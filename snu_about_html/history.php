0cfd19489b0c411338cdc5689b6929bb805bc6db
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-09-22 10:17:50 +0530

    modifications

--- snu_about_html/history.php
+++ snu_about_html/history.php
@@ -10,7 +10,9 @@
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 600 600" style="enable-background:new 0 0 600 600;" xml:space="preserve">
     <circle fill="none" stroke="#ffffff" stroke-miterlimit="10" cx="170" cy="230" r="150" style="stroke-dashoffset: 660.084; stroke-dasharray: 1885.96, 1895.96;"></circle>
     </svg>
-      
+    
+    <div class="dots"></div>
+    <div class="dots-fill"></div>
     
     <div class="container top-content animatedParent">
       <div class="content-block"><div class="breadcrumb">
