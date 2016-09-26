494ceb4b0ac128d053d397d49fae8187906535b8
baisaliinnoraft <baisali.gupta@innoraft.com>
2016-09-26 14:08:56 +0530

    small-changes

--- snu_about_html/history.php
+++ snu_about_html/history.php
@@ -5,7 +5,7 @@
   <div class="history-page">
   <section class="fixed-pad content-wrapper">
    
-      <div class="container ">
+      <div class="container position">
     
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 600 600" style="enable-background:new 0 0 600 600;" xml:space="preserve">
     <circle fill="none" stroke="#ffffff" stroke-miterlimit="10" cx="170" cy="230" r="150" style="stroke-dashoffset: 660.084; stroke-dasharray: 1885.96, 1895.96;"></circle>
@@ -33,7 +33,8 @@
     </div>
  
     
-    <div style="width:1600px;  position:absolute; left:-110px; top:393px; z-index:3;"> <div class="carousel-container">
+    <div class="slider-block"> 
+      <div class="carousel-container">
     
       <div id="carousel">
         <div class="carousel-feature">
